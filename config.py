497e5c80e803f2be9b7e7e8abb63152c6587e550
vincent-dinatale <122955042+vincent-dinatale@users.noreply.github.com>
2023-12-12 03:49:01 -0500

    API Keys

---
+++ config.py
@@ -0,0 +1,11 @@
+MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
+
+MAPBOX_TOKEN = "pk.eyJ1IjoidmluY2VudC1kaW5hdGFsZSIsImEiOiJjbG9xOXdqdW0wZWhmMmpxcW53Y3pmODl4In0.nAFnYdNPl3KF5ofxBB56lA"
+
+WEATHER_APIKEY = "3e9ea6188c09f37e38eaaa4152fc22c1"
+
+MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
+
+MBTA_API_KEY = "c6ad68631bd248198a962fb580fca8ab"
+
+TICKET_KEY = "Klr0BKzpA4DXGNOCGYzMJxB1Z1SYn2SO"
